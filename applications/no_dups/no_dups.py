0ed405d50f358381fb03e7c5552d9502dcdd0fb9
Krystal Guzman <kryguzman@gmail.com>
2020-07-06 16:15:43 -0700

    adds no dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,13 @@
 def no_dups(s):
-    # Your code here
+    words = s.split(" ")
+    words_seen = dict()
+    words_with_no_duplicates = []
 
-
+    for word in words:
+        if word not in words_seen:
+            words_seen[word] = 1
+            words_with_no_duplicates.append(word)
+    return " ".join(words_with_no_duplicates)
 
 if __name__ == "__main__":
     print(no_dups(""))

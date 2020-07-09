bc186ee510f7632ef33721c92f4f95b82dd2a816
Krystal Guzman <kryguzman@gmail.com>
2020-07-09 15:52:05 -0700

    adds another way of doing expensive seq

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -31,6 +31,15 @@ def expensive_seq(x, y, z):
 
     return result
 
+# def expensive_seq(x, y, z):
+#     if x <= 0:
+#         return y + z
+#     if (x, y, z) not in computed_results:
+#         computed_results[(x, y, z)] = expensive_seq(x - 1, y + 1, z) + \
+#                            expensive_seq(x - 2, y + 2, z * 2) + \
+#                            expensive_seq(x - 3, y + 3, z * 3)
+#     return computed_results[(x, y, z)]
+
 if __name__ == "__main__":
     for i in range(10):
         x = expensive_seq(i*2, i*3, i*4)
